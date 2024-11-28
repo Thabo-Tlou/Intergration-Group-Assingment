0fc18f3bc9d7ab02c25cb277ed3d704d569cb2b1
Hato444 <110167251+Hato444@users.noreply.github.com>
2024-11-28 17:08:53 +0200

    Routes

--- Group Assingment HR System/frontend/src/App.jsx
+++ Group Assingment HR System/frontend/src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
-import EmployeeList from '../components/EmployeeList'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import EmployeeManager from './Components/EmployeeManager';
+import EmployeeList from './Components/EmployeeList';
+import VehicleManager from './Components/VehicleManager';
+import ErrorBoundary from './Components/ErrorBoundary';
+import './styles/App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    <EmployeeList />
-    </>
-  )
-}
+    <ErrorBoundary>
+      <Router>
+        <div className="app-container">
+          {/* Simplified Navigation Bar */}
+          <nav className="navigationWrapper">
+            <div className="logoWrapper">
+              <span className="stylish">AWY</span>
+              <span className="logo">HR SYSTEM</span>
+              <span className = "tagline">Sounds Of Sensation</span>
+            </div>
+            <ul className="navigation">
+              <li className="parent">
+                <Link className="link" to="/employees">Staff Information</Link>
+              </li>
+              <li className="parent">
+                <Link className="link" to="/employee-manager">Adding New Recruits</Link>
+              </li>
+              <li className="parent">
+                <Link className="link" to="/vehicles">Vehicle Procurement</Link>
+              </li>
+            </ul>
+          </nav>
 
-export default App
+          {/* Routing for Application Pages */}
+          <Routes>
+            <Route path="/" element={<Navigate to="/employees" />} />
+            <Route path="/employees" element={<EmployeeList />} />
+            <Route path="/employee-manager" element={<EmployeeManager />} />
+            <Route path="/vehicles" element={<VehicleManager />} />
+          </Routes>
+        </div>
+      </Router>
+    </ErrorBoundary>
+  );
+};
+
+export default App;
